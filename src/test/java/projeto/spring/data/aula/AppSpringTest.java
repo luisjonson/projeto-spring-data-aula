052efce4dd30a77fe352e081c4ffd3464fa07af6
jonson <luizlindonjonson@yahoo.com.br>
2020-01-25 22:53:36 -0300

    Add no git remoto

---
+++ src/test/java/projeto/spring/data/aula/AppSpringTest.java
@@ -0,0 +1,23 @@
+package projeto.spring.data.aula;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:META-INF/spring-config.xml" })
+public class AppSpringTest {
+
+	@Test
+	public void testeIsert() {
+		try {
+
+			System.out.println("conexão bem sucedida...");
+		} catch (Exception e) {
+			e.printStackTrace();
+			// TODO: handle exception
+		}
+	}
+
+}
