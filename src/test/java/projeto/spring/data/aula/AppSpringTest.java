5f2cec33ba6953d8e82feef3540bafd668a2d650
jonson <luizlindonjonson@yahoo.com.br>
2020-01-31 21:05:24 -0300

    consulta por ID com

--- src/test/java/projeto/spring/data/aula/AppSpringTest.java
+++ src/test/java/projeto/spring/data/aula/AppSpringTest.java
@@ -1,19 +1,46 @@
 package projeto.spring.data.aula;
 
+import java.util.Optional;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import projeto.spring.data.aula.dao.InterfaceSpringDataUser;
+import projeto.spring.data.aula.model.UsuarioSpringData;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:META-INF/spring-config.xml" })
 public class AppSpringTest {
 
+	@Autowired
+	private InterfaceSpringDataUser interfaceSpringDataUser;
+
 	@Test
 	public void testeIsert() {
+		
+		UsuarioSpringData usuarioSpringData = new  UsuarioSpringData();
+		usuarioSpringData.setNome("Spring");
+		usuarioSpringData.setEmail("spring@yahoo.com.br");
+		usuarioSpringData.setIdade(30);
+		usuarioSpringData.setLogin("teste123");
+		usuarioSpringData.setSenha("12345");
+		
+		interfaceSpringDataUser.save(usuarioSpringData);
 
-		System.out.println("conexão bem sucedida...");
+		System.out.println("Usuario no banco " + interfaceSpringDataUser.count());
 
+	}
+	
+	@Test
+	public void testeConsulta() {
+		
+		Optional<UsuarioSpringData> usuarioSpringData = interfaceSpringDataUser.findById(2L);		
+		System.out.println("Usuario é: " + usuarioSpringData.get().getNome());
+		System.out.println("Idade: " + usuarioSpringData.get().getIdade());
+		System.out.println("Email: " + usuarioSpringData.get().getEmail());
 	}
 
 }
