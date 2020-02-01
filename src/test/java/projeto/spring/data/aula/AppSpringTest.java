5fded2eb02e6b87c02b7e3403347be632deb3058
jonson <luizlindonjonson@yahoo.com.br>
2020-01-31 21:28:12 -0300

    consultar todos os registro no banco.

--- src/test/java/projeto/spring/data/aula/AppSpringTest.java
+++ src/test/java/projeto/spring/data/aula/AppSpringTest.java
@@ -37,10 +37,26 @@ public class AppSpringTest {
 	@Test
 	public void testeConsulta() {
 		
+		
 		Optional<UsuarioSpringData> usuarioSpringData = interfaceSpringDataUser.findById(2L);		
 		System.out.println("Usuario é: " + usuarioSpringData.get().getNome());
 		System.out.println("Idade: " + usuarioSpringData.get().getIdade());
 		System.out.println("Email: " + usuarioSpringData.get().getEmail());
+	}
+	@Test
+	public void testeConsultaTodos() {
+		
+		Iterable<UsuarioSpringData> lista = interfaceSpringDataUser.findAll();
+		
+		for (UsuarioSpringData usuarioSpringData : lista) {
+			
+			System.out.println("Nome: " + usuarioSpringData.getNome());
+			System.out.println("Idade: " + usuarioSpringData.getIdade());
+			System.out.println("Email: " +usuarioSpringData.getEmail());
+			System.out.println("Senha: " + usuarioSpringData.getSenha());
+			System.out.println("--------------------------------------------");
+			
+		}
 	}
 
 }
